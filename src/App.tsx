80e4c08f47fd57451539f38641b415b4a3e86c93
yanngdev <dev@yanng.me>
2022-11-29 09:06:26 +0100

    v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Clock from "react-clock";
+import "the-new-css-reset/css/reset.css";
+import "react-clock/dist/Clock.css";
+
+import "./App.scss";
 
 function App() {
+  const [value, setValue] = React.useState(new Date());
+
+  const staticValue = React.useMemo<Date>(() => new Date(), []);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const isDarkTheme = value.getHours() > 21 || value.getHours() < 7;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={`main ${isDarkTheme ? "dark" : "light"}`}>
+      <div className="left">
+        <div className="date">
+          <div className="text day-text">
+            {value.toLocaleString(window.navigator.language, {
+              weekday: "long",
+            })}
+          </div>
+          <div className="text date-text">
+            {value.toLocaleString(window.navigator.language, {
+              day: "numeric",
+            })}
+            <br />
+            {value.toLocaleString(window.navigator.language, {
+              month: "long",
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="text time-text">
+          {value.toLocaleString(window.navigator.language, {
+            minute: "numeric",
+            hour: "numeric",
+          })}
+        </div>
+
+        <Clock
+          value={value}
+          size={560}
+          className="clock"
+          hourHandWidth={8}
+          hourHandLength={60}
+          hourHandOppositeLength={0}
+          hourMarksWidth={6}
+          hourMarksLength={8}
+          minuteHandWidth={8}
+          minuteMarksWidth={4}
+          minuteMarksLength={6}
+          minuteHandLength={60}
+          minuteHandOppositeLength={0}
+          secondHandWidth={6}
+          secondHandLength={100}
+          secondHandOppositeLength={16}
+        />
+
+        <Clock
+          value={staticValue}
+          size={536}
+          className="clock-hours"
+          hourHandWidth={0}
+          hourMarksWidth={0}
+          renderHourMarks
+          renderMinuteHand={false}
+          renderMinuteMarks={false}
+          renderSecondHand={false}
+          renderNumbers
+        />
+
+        <div className="dot"></div>
+      </div>
+    </main>
   );
 }
 
